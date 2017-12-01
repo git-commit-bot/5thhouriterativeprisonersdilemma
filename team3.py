774654aeda1b7e737ff87ba7b1025b82041f1e87
Adampass1102 <adam.passalacqua@avondaleschools.org>
2017-12-01 11:57:11 -0500

    Team3
    Team3's changes

--- team3.py
+++ team3.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Team3' # Only 10 chars displayed.
+strategy_name = 'Betray'
+strategy_description = 'Always Betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
